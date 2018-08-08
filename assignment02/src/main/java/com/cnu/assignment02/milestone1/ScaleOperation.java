745718f1e5a38f6459afc5275164b458592b3b8e
Arpit Singla <arpit.singla@codenation.co.in>
2018-08-09 00:41:10 +0400

    Fix Scale Algo

--- assignment02/src/main/java/com/cnu/assignment02/milestone1/ScaleOperation.java
+++ assignment02/src/main/java/com/cnu/assignment02/milestone1/ScaleOperation.java
@@ -4,36 +4,38 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class ScaleOperation extends ImageOperation {
-    public int height;
-    public int width;
+    public double height;
+    public double width;
+    private int widthInt;
+    private int heightInt;
 
     private void fixResolution(BufferedImage inputImage) {
-        if (width == 0 && height == 0) {
-            width = inputImage.getWidth();
-            height = inputImage.getHeight();
+        if (widthInt == 0 && heightInt == 0) {
+            widthInt = inputImage.getWidth();
+            heightInt = inputImage.getHeight();
         }
-        else if (width == 0) {
-            width = (int) Math.floor((double)inputImage.getWidth() / (double)inputImage.getHeight() * (double)height);
+        else if (widthInt == 0) {
+            widthInt = (int) Math.floor((double)inputImage.getWidth() / (double)inputImage.getHeight() * (double)heightInt);
         }
-        else if (height == 0) {
-            height = (int) Math.floor((double)inputImage.getHeight() / (double)inputImage.getWidth() * (double)width);
+        else if (heightInt == 0) {
+            heightInt = (int) Math.floor((double)inputImage.getHeight() / (double)inputImage.getWidth() * (double)widthInt);
         }
     }
 
     private BufferedImage scale(BufferedImage inputImage) {
         int type = inputImage.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : inputImage.getType();
         fixResolution(inputImage);
-        BufferedImage operatedImage = new BufferedImage(width, height, type);
+        BufferedImage operatedImage = new BufferedImage(widthInt, heightInt, type);
         Graphics2D g = operatedImage.createGraphics();
-        g.drawImage(inputImage, 0, 0, width, height, null);
+        g.drawImage(inputImage, 0, 0, widthInt, heightInt, null);
         g.dispose();
         return operatedImage;
     }
 
     @Override
     public BufferedImage start(BufferedImage inputImage) {
-        width = width*inputImage.getWidth();
-        height = height*inputImage.getHeight();
+        widthInt = (int) (width * inputImage.getWidth());
+        heightInt = (int) (height * inputImage.getHeight());
         return scale(inputImage);
     }
 }
