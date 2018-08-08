642a3313f241f2c9d9137ea78632a08c047ca237
Arpit Singla <arpit.singla@codenation.co.in>
2018-08-09 00:22:08 +0400

    Fix Rotation Algo

--- assignment02/src/main/java/com/cnu/assignment02/RotateOperation.java
+++ assignment02/src/main/java/com/cnu/assignment02/RotateOperation.java
@@ -1,30 +1,28 @@
 package com.cnu.assignment02;
 
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 
 public class RotateOperation extends ImageOperation {
     public int degrees;
 
-    public BufferedImage rotateClockWise(BufferedImage inputImage) {
-        int width  = inputImage.getWidth();
-        int height = inputImage.getHeight();
+    public BufferedImage rotateImage(BufferedImage inputImage) {
         int type = inputImage.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : inputImage.getType();
-        BufferedImage   operatedImage = new BufferedImage( height, width, type );
+        BufferedImage operatedImage = new BufferedImage(inputImage.getWidth(), inputImage.getHeight(), type);
 
-        for( int i=0 ; i < width ; i++ )
-            for( int j=0 ; j < height ; j++ )
-                operatedImage.setRGB( height-1-j, i, inputImage.getRGB(i,j) );
+        AffineTransform tx = new AffineTransform();
+        tx.rotate((double)degrees/180.0*Math.PI, inputImage.getWidth() / 2, inputImage.getHeight() / 2);
 
+        AffineTransformOp op = new AffineTransformOp(tx,
+                AffineTransformOp.TYPE_BICUBIC);
+        operatedImage = op.filter(inputImage, operatedImage);
         return operatedImage;
     }
 
     @Override
     public BufferedImage start(BufferedImage inputImage) {
-        BufferedImage operatedImage = inputImage;
-        for (int i=0; i < (degrees/90)%4; i++) {
-            operatedImage = rotateClockWise(operatedImage);
-        }
-
+        BufferedImage operatedImage = rotateImage(inputImage);
         return operatedImage;
     }
 
