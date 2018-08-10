8120672c7b06007fbbfba85ae048052e9b4bd51b
Arpit Singla <arpit.singla@codenation.co.in>
2018-08-10 11:55:46 +0400

    Expose 9998 on client to show status

--- assignment01b/milestone1/client/client.sh
+++ assignment01b/milestone1/client/client.sh
@@ -1,17 +1,19 @@
 set -e;
 
-clientdata_path="/clientdata/x";
-server_hostname=$SERVER_HOSTNAME;
-port=$PORT;
+while true; do
+	clientdata_path="/clientdata/x";
+	server_hostname=$SERVER_HOSTNAME;
+	port=$PORT;
 
-data="$(nc $server_hostname $port)";
-checksum_received="$(echo $data | cut -d'|' -f1)";
-printf "$data" | awk '{split($0,a,"|"); printf a[2]}' > "$clientdata_path";
-checksum_calculated="$(md5sum "$clientdata_path" | cut -d' ' -f1)";
+	data="$(nc $server_hostname $port)";
+	checksum_received="$(echo $data | cut -d'|' -f1)";
+	printf "$data" | awk '{split($0,a,"|"); printf a[2]}' > "$clientdata_path";
+	checksum_calculated="$(md5sum "$clientdata_path" | cut -d' ' -f1)";
 
-if [ "$checksum_calculated" == "$checksum_received" ]
-then
-	echo "Success!";
-else
-	echo "Failed!";
-fi;
+	if [ "$checksum_calculated" == "$checksum_received" ]
+	then
+		echo "Success!" | nc -l -p 9998;
+	else
+		echo "Failed!" | nc -l -p 9998;
+	fi;
+done;
