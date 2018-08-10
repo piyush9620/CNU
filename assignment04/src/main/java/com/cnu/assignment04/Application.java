9515a1869e0c0114a97838e8f75f69473fc835a1
Arpit Singla <arpit.singla@codenation.co.in>
2018-08-11 02:58:27 +0400

    Adds logs

--- assignment04/src/main/java/com/cnu/assignment04/Application.java
+++ assignment04/src/main/java/com/cnu/assignment04/Application.java
@@ -7,11 +7,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setMaxPayloadLength(10000);
+        loggingFilter.setIncludePayload(true);
+        return loggingFilter;
     }
 }
