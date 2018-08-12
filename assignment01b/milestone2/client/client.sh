d3f3a398a3a9a542641fc4e147f1a85664d26726
Arpit Singla <arpit.singla@codenation.co.in>
2018-08-12 11:01:02 +0400

    Should work

--- assignment01b/milestone2/client/client.sh
+++ assignment01b/milestone2/client/client.sh
@@ -1,16 +1,20 @@
 set -e;
 
-clientdata_path="/clientdata/x";
+temp_data_path="/tmp/data"
+config_map="/tmp/configMap.yml";
 server_hostname=$SERVER_HOSTNAME;
 port=$PORT;
 
-data="$(nc $server_hostname $port)";
+data="$(timeout -t 1 nc $server_hostname $port)";
 checksum_received="$(echo $data | cut -d'|' -f1)";
-printf "$data" | awk '{split($0,a,"|"); printf a[2]}' > "$clientdata_path";
-checksum_calculated="$(md5sum "$clientdata_path" | cut -d' ' -f1)";
+printf "$data" | awk '{split($0,a,"|"); printf a[2]}' > "$temp_data_path";
+checksum_calculated="$(md5sum "$temp_data_path" | cut -d' ' -f1)";
 
 if [ "$checksum_calculated" == "$checksum_received" ]
 then
+	cp "/client/partialConfigMap.yml" "$config_map";
+	echo "$checksum_calculated: $(cat $temp_data_path)" >> "$config_map";
+	kubectl apply -f $config_map --namespace cnu2018-dev;
 	echo "Success!";
 else
 	echo "Failed!";
