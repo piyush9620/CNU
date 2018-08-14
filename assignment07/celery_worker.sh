08e0744c5347d953ab85576509f9663f9312bd39
Arpit Singla <arpit.singla@codenation.co.in>
2018-08-15 03:15:02 +0400

    Adds flower

--- assignment07/celery_worker.sh
+++ assignment07/celery_worker.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-celery -A assignment07 worker -l info;
+celery -A assignment07 worker -l info &
+
+celery -A assignment07 flower --port=5555 --basic_auth=$CELERY_USERNAME:$CELERY_PASSWORD;
