238af248b2a2a27f715bc5e256485c89c500fc80
Arpit Singla <arpit.singla@codenation.co.in>
2018-08-14 20:28:16 +0400

    Adds redis dockerfile

--- assignment07/assignment07/sample.config.py
+++ assignment07/assignment07/sample.config.py
@@ -3,8 +3,8 @@ import os
 config = {
     "DATABASE_HOSTNAME": os.environ.get('DB_HOST', "localhost"),
     "DATABASE_USER": os.environ.get('DB_USER', ""),
-    "DATABASE_PASSWORD": os.environ('DB_PASS', ""),
-    "DATABASE_NAME": os.environ('DB_NAME', "assignment07"),
-    "CELERY_BROKER": os.environ('CELERY_BROKER', "redis://localhost:6379/0"),
-    "CELERY_BACKEND": os.environ('CELERY_BACKEND', "redis://localhost:6379/0")
+    "DATABASE_PASSWORD": os.environ.get('DB_PASS', ""),
+    "DATABASE_NAME": os.environ.get('DB_NAME', "assignment07"),
+    "CELERY_BROKER": os.environ.get('CELERY_BROKER', "redis://localhost:6379/0"),
+    "CELERY_BACKEND": os.environ.get('CELERY_BACKEND', "redis://localhost:6379/0")
 }
