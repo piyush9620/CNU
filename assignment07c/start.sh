085c4057199d2fc80eff01ac7b803c86d9b0628f
Arpit Singla <arpit.singla@codenation.co.in>
2018-08-15 19:08:53 +0400

    Fix minor issues in dockerfile

--- assignment07c/start.sh
+++ assignment07c/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Start Gunicorn processes
+aws s3 cp s3://cnu-2k18/arpit/a07c/config.py assignment07c/config.py
+
 python3 manage.py collectstatic
 
 python3 manage.py migrate
@@ -8,9 +10,6 @@ python3 manage.py migrate
 nginx
 
 echo Starting Gunicorn.
-exec gunicorn assignment07.wsgi:application \
+exec gunicorn assignment07c.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 3
-
-
-
